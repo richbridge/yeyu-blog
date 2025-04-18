56a5f17da2794c58c62a02bf334be5a158da9373
Ye Yu <nearjilt@gmail.com>
2025-04-18 21:13:27 +0800

    feat: note tag 容器修改为无限滚动组件

--- modules/admin/page/admin-note-page/internal/note-tags-container.tsx
+++ modules/admin/page/admin-note-page/internal/note-tags-container.tsx
@@ -5,9 +5,32 @@ import { getTagsOnNote } from '@/actions/notes'
 import { NoteTag } from '@prisma/client'
 import { NoteTagItemToggle } from '@/components/shared/tag-item-toggle'
 import { toast } from 'sonner'
+import {
+  Carousel,
+  CarouselApi,
+  CarouselContent,
+  CarouselItem,
+} from '@/components/ui/carousel'
+import { cn } from '@/lib/utils'
 
 export function NoteTagsContainer() {
   const [tags, setTags] = useState<NoteTag['tagName'][]>([])
+  const [api, setApi] = useState<CarouselApi>()
+  const [current, setCurrent] = useState(1)
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    if (!api) {
+      return
+    }
+
+    setCount(api.scrollSnapList().length)
+    setCurrent(api.selectedScrollSnap() + 1)
+
+    api.on('select', () => {
+      setCurrent(api.selectedScrollSnap() + 1)
+    })
+  }, [api])
 
   useEffect(() => {
     const fetchTags = async () => {
@@ -24,10 +47,43 @@ export function NoteTagsContainer() {
   }, [])
 
   return (
-    <section className="w-full flex gap-2">
-      {tags.map(tag => (
-        <NoteTagItemToggle key={tag.toLowerCase()} tag={tag} />
-      ))}
+    <section className="relative w-full">
+      {/* 左侧 fade 遮罩 */}
+      <span
+        className={cn(
+          'absolute left-0 top-0 bottom-0 w-12 z-10',
+          'bg-gradient-to-r from-white/80 to-transparent dark:from-black/60',
+          'pointer-events-none transition-colors duration-300 ease-in-out',
+          current === 1 && 'hidden',
+        )}
+      />
+      <Carousel
+        opts={{
+          align: 'start',
+          dragFree: true,
+        }}
+        setApi={setApi}
+        // * 魔法值，后序考虑使用计算得到
+        className="w-full max-w-[96vw]"
+      >
+        <CarouselContent>
+          {tags.map(tag => (
+            <CarouselItem className="basis-auto" key={tag.toLowerCase()}>
+              <NoteTagItemToggle tag={tag} />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
+
+      {/* 右侧 fade 遮罩 */}
+      <span
+        className={cn(
+          'absolute right-0 top-0 bottom-0 w-12 z-10',
+          'bg-gradient-to-l from-white/80 to-transparent dark:from-black/60',
+          'pointer-events-none transition-colors duration-300 ease-in-out',
+          current === count && 'hidden',
+        )}
+      />
     </section>
   )
 }
