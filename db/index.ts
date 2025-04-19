94dc1d2611d135139ed5a75651f65d55ca265064
Ye Yu <nearjilt@gmail.com>
2025-04-19 12:02:51 +0800

    refactor: 重新创建 prisma 实例

--- db/index.ts
+++ db/index.ts
@@ -1,90 +1,101 @@
 import { PrismaClient } from '@prisma/client'
-import fs from 'fs'
 
-export const prisma = new PrismaClient()
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
 
-const filePath = `md/2.md`
-const fileContent = fs.readFileSync(filePath, 'utf-8')
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+// * 测试脚本
+// import { PrismaClient } from '@prisma/client'
+// import fs from 'fs'
+
+// export const prisma = new PrismaClient()
+
+// const filePath = `md/2.md`
+// const fileContent = fs.readFileSync(filePath, 'utf-8')
+// // const randomTag = [
+// //   'React',
+// //   'Vue',
+// //   'TypeScript',
+// //   'Go',
+// //   'Nextjs',
+// //   'Remix',
+// //   'JavaScript',
+// // ] as const
+
 // const randomTag = [
-//   'React',
-//   'Vue',
-//   'TypeScript',
-//   'Go',
-//   'Nextjs',
-//   'Remix',
-//   'JavaScript',
+//   '读书',
+//   '日常',
+//   '好累',
+//   '测试',
+//   '现在21:41',
+//   '写一天了',
+//   '还在测试!',
 // ] as const
 
-const randomTag = [
-  '读书',
-  '日常',
-  '好累',
-  '测试',
-  '现在21:41',
-  '写一天了',
-  '还在测试!',
-] as const
+// async function main() {
+//   // await mockData(100)
+//   // await mockEchoData(50)
+//   // console.log('over')
+// }
 
-async function main() {
-  // await mockData(100)
-  // await mockEchoData(50)
-  // console.log('over')
-}
+// async function mockEchoData(count: number) {
+//   return Promise.all(
+//     Array(count)
+//       .fill(0)
+//       .map(async _ => {
+//         await prisma.echo.create({
+//           data: {
+//             content: `echo 测试 ${generateRandomString()}`,
+//             reference: `reference ${generateRandomString()}`,
+//           },
+//         })
+//       }),
+//   )
+// }
 
-async function mockEchoData(count: number) {
-  return Promise.all(
-    Array(count)
-      .fill(0)
-      .map(async _ => {
-        await prisma.echo.create({
-          data: {
-            content: `echo 测试 ${generateRandomString()}`,
-            reference: `reference ${generateRandomString()}`,
-          },
-        })
-      }),
-  )
-}
+// async function mockData(count: number) {
+//   return Promise.all(
+//     Array(count)
+//       .fill(0)
+//       .map(async _ => {
+//         await prisma.note.create({
+//           data: {
+//             title: `note 测试 ${generateRandomString()}`,
+//             slug: `life-style-${generateRandomString()}`,
+//             tags: {
+//               connectOrCreate: getRandomTags().map(tagName => ({
+//                 where: { tagName },
+//                 create: { tagName },
+//               })),
+//             },
+//             content: fileContent,
+//           },
+//         })
+//       }),
+//   )
+// }
 
-async function mockData(count: number) {
-  return Promise.all(
-    Array(count)
-      .fill(0)
-      .map(async _ => {
-        await prisma.note.create({
-          data: {
-            title: `note 测试 ${generateRandomString()}`,
-            slug: `life-style-${generateRandomString()}`,
-            tags: {
-              connectOrCreate: getRandomTags().map(tagName => ({
-                where: { tagName },
-                create: { tagName },
-              })),
-            },
-            content: fileContent,
-          },
-        })
-      }),
-  )
-}
+// function generateRandomString(length: number = 8): string {
+//   return Math.random()
+//     .toString(36)
+//     .slice(2, 2 + length)
+// }
+// function getRandomTags(): string[] {
+//   const shuffled = [...randomTag].sort(() => Math.random() - 0.5) // 随机打乱
+//   const count = Math.floor(Math.random() * 3) + 1 // 随机选 1~3 个
+//   return shuffled.slice(0, count)
+// }
 
-function generateRandomString(length: number = 8): string {
-  return Math.random()
-    .toString(36)
-    .slice(2, 2 + length)
-}
-function getRandomTags(): string[] {
-  const shuffled = [...randomTag].sort(() => Math.random() - 0.5) // 随机打乱
-  const count = Math.floor(Math.random() * 3) + 1 // 随机选 1~3 个
-  return shuffled.slice(0, count)
-}
-
-main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async e => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+// main()
+//   .then(async () => {
+//     await prisma.$disconnect()
+//   })
+//   .catch(async e => {
+//     console.error(e)
+//     await prisma.$disconnect()
+//     process.exit(1)
+//   })
