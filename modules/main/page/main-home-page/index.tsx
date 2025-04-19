f01274a5f7fe46c344cbd02e1135f78fd2b4ba3a
Ye Yu <nearjilt@gmail.com>
2025-04-19 18:40:14 +0800

    feat: 新增首页动画

--- modules/main/page/main-home-page/index.tsx
+++ modules/main/page/main-home-page/index.tsx
@@ -2,14 +2,23 @@ import BioSection from './internal/bio-section'
 import EchoCard from './internal/echo-card'
 import TechStack from './internal/tech-stack'
 import YeAvatar from './internal/ye-avatar'
+import * as motion from 'motion/react-client'
 
 export default function MainLayoutContainer() {
   return (
-    <main className="flex flex-col justify-center items-center gap-6 py-4 overflow-hidden">
+    <motion.main
+      className="flex flex-col justify-center items-center gap-6 py-4 overflow-hidden"
+      initial={{ opacity: 0, y: 60 }}
+      animate={{ opacity: 1, y: [-10, 0] }}
+      transition={{
+        duration: 0.6,
+        ease: 'easeOut',
+      }}
+    >
       <YeAvatar />
       <BioSection />
       <EchoCard />
       <TechStack />
-    </main>
+    </motion.main>
   )
 }
