e35f309b44433c1ba3d393341151cab3fa899db7
Ye Yu <nearjilt@gmail.com>
2025-04-21 12:19:48 +0800

    refactor: 使用 link 引入字体资源替换 @import

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,12 @@ export default function RootLayout({
   return (
     // * 添加亮暗切换必报水合错误错误😅
     <html lang="zh-CN" suppressHydrationWarning>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/lxgw-wenkai-webfont/1.7.0/lxgwwenkai-regular.min.css"
+        />
+      </head>
       <body className="font-ye-font">
         <ThemeProvider
           attribute="class"
