13c7373d65bd2d2ada17e1864fb7ca35adbaf771
Ye Yu <nearjilt@gmail.com>
2025-04-26 11:14:12 +0800

    feat: 集成 vercel 数据分析

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import { ThemeProvider } from '@/components/ui/theme-provider'
 import { LXGW, metadata } from '@/config/constant'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import './globals.css'
 
 export { metadata }
@@ -21,6 +23,8 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
